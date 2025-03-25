e539979e28abb8cfec927db0dd7c2cdd6b2dc97e
Marcin Polonis <polo@desmart.com>
2025-03-25 23:37:05 +0100

    feat: add routing of the project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,39 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-
-const routes: Array<RouteRecordRaw> = []
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router"
+import StopsAndTimeSection from "@/components/StopsAndTimeSection.vue"
+import BusStopsList from "@/components/BusStopsList.vue"
+import TimeList from "@/components/TimeList.vue"
+import AllBusStopsList from "@/components/AllBusStopsList.vue"
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    component: StopsAndTimeSection,
+    children: [
+      {
+        path: "lines/:line",
+        name: "line",
+        component: BusStopsList,
+        props: true,
+        children: [
+          {
+            path: ":stop",
+            name: "time",
+            component: TimeList,
+            props: true,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: "/stops",
+    name: "stops",
+    component: AllBusStopsList,
+  },
+  ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 })
 
 export default router
